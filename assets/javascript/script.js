21f5b3eebec69df0ac7e13a4af359ccfc4d1fce5
anishasnet <anishasnet@gmail.com>
2020-04-04 18:38:16 -0700

    set up welcome page

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+var questions = [
+"Commonly used data types do NOT include:",
+"The condition in an if/else statement is enclosed with _______.",
+"Arrays in JavaScript can be used to store _______.",
+"String values must be enclosed within _______ when being assigned to variables.",
+"A very useful tool used during development and debugging for printing conent to the debugger is:",]
